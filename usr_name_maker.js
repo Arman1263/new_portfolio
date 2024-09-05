a96d256c38cc8a0cc7acd814502499d39e793dc8
Arman Amir Shikalgar <139204643+Arman1263@users.noreply.github.com>
2024-09-05 12:19:11 +0530

    Add files via upload

---
+++ usr_name_maker.js
@@ -0,0 +1,4 @@
+let usr = prompt("Enter the value");
+let res = "@" + usr + usr.length;
+console.log(`THE ENTERED INPUT IS ${usr}`);
+console.log(`CONGRATULATIONS YOU GOT THIS ${res} TO YOUR ACCOUNT `)
